8faa9fba5591db87c0dbfddb542f07f449041047
heather propes <hbuch4@yahoo.com>
2025-07-16 16:37:34 -0400

    want to add checkbox to ask ai

--- backend/routes.py
+++ backend/routes.py
@@ -8,7 +8,7 @@ from backend.models.question import Question
 from backend.models.answer import Answer
 from backend.models.schemas import QuestionSchema
 from backend.models.user import User
-from backend.models import db
+from backend.models import db, user
 from dotenv import load_dotenv
 import spacy
 import os
@@ -22,51 +22,42 @@ openai = OpenAI() #magically gets them from environment
 
 # returns json of question, ai answers
 # @bp.route("/askai", methods=['POST'])
-# def ask_ai():
-#     data = request.json
-#     question_text = data.get("question")
-#
-#     # Load spaCy English model
-#     nlp = spacy.load("en_core_web_sm")
-#
-#     # Process the text using spaCy
-#     lex = nlp(question_text)
-#     filtered_lex = [token.text for token in lex if not not token.is_stop]
-#     question_name = ' '.join(filtered_lex)
-#     existing = Question.query.filter_by(text=question_text).first()
-#
-#     answers = []
-#     # look for it in gpt, save question and gpt answer
-#     question = Question(name=question_name, text=question_text, author_id=1, dateCreated=datetime.now())
-#     db.session.add(question)
-#     db.session.flush()
-#     # db.session.commit()
-#     # gpt answer stuff figure out how to use it
-#     response = openai.chat.completions.create(
-#         model="gpt-4",
-#         messages=[{"role": "user", "content": question_text}]
-#     )
-#     gpt_resp = response.choices[0].message.content
-#     gpt_lex = nlp(gpt_resp)
-#     filtered_gpt_lex = [token.text for token in gpt_lex if not not token.is_stop]
-#     gpt_resp_name = ' '.join(filtered_gpt_lex[:3])
-#     gpt_answer = Answer(name=gpt_resp_name, text=gpt_resp, source="ai", author_id=3)
-#     db.session.add(gpt_answer)
-#     # db.session.flush()
-#     db.session.commit()
-#
-#     aiAnswerMaps = QuestionAnswer.query.filter_by(question_id=question.id).all()
-#     for i, qa_data in enumerate(aiAnswerMaps):
-#         # print('qa ', *qa_data, sep='\n')
-#         answer = Answer.query.filter_by(id=qa_data.answer_id).first()
-#         answers.append({
-#             "name": answer.name,
-#             "text": answer.text,
-#             "dateCreated":answer.dateCreated
-#         })
-#     # answer = Answer.query.filter_by(id=2).first()
-#
-#     return jsonify({"answer": answers})
+@bp.route('/question/ai/ask',methods=['POST'])
+def ask_ai():
+    data = request.json
+    question_text = data.get("questionText")
+    user_id = data.get("userId")
+    print('question text: ', question_text, 'user_id: ', user_id)
+    ai_user = User.query.filter_by(user_id=user_id).first()
+    print('ai_user_name: ', ai_user.fullName)
+    # existing = Question.query.filter_by(id=question_id).filter_by(user_id=user_id).order_by(desc(Question.dateCreated)).first()
+
+    # single_schema = QuestionSchema(many=False)
+    # if existing:
+    #     question_result = single_schema.dump(existing)
+    #     print('existing question json: ', json.dumps(question_result, indent=4))
+
+    # gpt answer stuff figure out how to use it
+    response = openai.chat.completions.create(
+        model="gpt-4",
+        messages=[{"role": "user", "content": question_text}]
+    )
+    gpt_resp = response.choices[0].message.content
+    gpt_lex = remove_stopwords(gpt_resp)
+    gpt_resp_name = ' '.join(gpt_lex[:3])
+    # gpt_answer = Answer(name=gpt_resp_name, text=gpt_resp, user_id=user_id, )
+    # db.session.add(gpt_answer)
+    # db.session.flush()
+    # db.session.commit()
+    answer = {
+        "name": gpt_resp_name,
+        "text": gpt_resp,
+        "dateCreated": datetime.now(),
+        "user_name": ai_user.fullName,
+        "user_type": ai_user.type,
+        "user_id": ai_user.id
+    }
+    return jsonify({"answer": answer})
 
 # returns json of question, humans answers
 @bp.route("/questions/search", methods=['POST'])
@@ -74,12 +65,8 @@ def ask():
     data = request.json
     question_text = data.get("questionText")
     print('question text: ', question_text)
-    # Load spaCy English model
-    nlp = spacy.load("en_core_web_sm")
 
-    # Process the text using spaCy
-    lex = nlp(question_text)
-    filtered_lex = [token.text for token in lex if not token.is_stop]
+    filtered_lex = remove_stopwords(question_text)
     question_name = ' '.join(filtered_lex)
 
     existing = Question.query.filter_by(text=question_text).order_by(desc(Question.dateCreated)).first()
@@ -106,6 +93,14 @@ def ask():
 
     return jsonify({ "question": question_result, "similarQuestions": similar_result })
 
+def remove_stopwords(raw_text):
+    # Load spaCy English model
+    nlp = spacy.load("en_core_web_sm")
+
+    # Process the text using spaCy
+    lex = nlp(raw_text)
+    filtered_lex = [token.text for token in lex if not token.is_stop]
+    return filtered_lex
 
 
 
